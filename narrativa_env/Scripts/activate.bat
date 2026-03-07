3bf8bfdfdc2819a46b4f67ae220431be28933c80
khushik19 <katiyarkhushi19@gmail.com>
2026-03-07 22:48:04 +0530

    j

--- narrativa_env/Scripts/activate.bat
+++ narrativa_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\katiy\OneDrive\Desktop\Canteen Crave\Autonomous-Narrative-Engine\narrativa_env"
+set "VIRTUAL_ENV=C:\Autonomous-Narrative-Engine\narrativa_env"
 
 if not defined PROMPT set PROMPT=$P$G
 
